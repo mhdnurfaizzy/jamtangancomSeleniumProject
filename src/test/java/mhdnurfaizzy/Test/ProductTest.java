195c52ec4e6d239a7a0d3c2ade7d2f225a0d8b26
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-04-28 12:28:54 +0700

    delete func login Test on ProductTest

--- src/test/java/mhdnurfaizzy/Test/ProductTest.java
+++ src/test/java/mhdnurfaizzy/Test/ProductTest.java
@@ -14,18 +14,9 @@ import java.util.List;
 
 public class ProductTest extends baseTesting {
 
-    @DataProvider
-    public static Object[][] getData() throws IOException {
-        List<HashMap<String, String>> data = getDataJsonToMap(System.getProperty("user.dir") + "src/main/java/mhdnurfaizzy/data/Purchase.json");
-        return new Object[][] { {data.get(0)} };
-    }
 
-    @Test(dataProvider= "getData",groups= {"Regression"})
-    public void addProductToCart(HashMap<String, String> input) {
-        //Login
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.loginApplication(input.get("email"), input.get("password"));
-
+    @Test()
+    public void addProductToCart() {
         //home page
         HomePage homePage = new HomePage(driver);
         homePage.popUpAfterlogin();
